f188e73fb94b71ca200bd4c13c9184cbb7ce2198
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-05-20 15:50:58 +0700

    edit using getStaticProps and container component

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 
+import { Head } from '../components'
+
 import styles from '../styles/Home.module.css'
 
 const container = ({ children, title }) => {
--- src/pages/using-getstaticprops.jsx
+++ src/pages/using-getstaticprops.jsx
@@ -1,49 +1,23 @@
 import React from 'react'
 import Image from 'next/image'
 
-import { Head, Card } from '../components'
+import { Card, Container } from '../components'
 
 import styles from '../styles/Home.module.css'
 
 const usingGetstaticprops = ({ data }) => {
     return (
-        <div className={styles.container}>
-            <Head title='Using getStaticProps' />
+        <Container title='Using getStaticProps'>
+            <h1 className={styles.title}>Using getStaticProps</h1>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>Using getStaticProps</h1>
-
-                <div className={styles.cardWrapper}>
-                    {data.map((item, index) => {
-                        return (
-                            <Card
-                                key={index}
-                                title={item.title}
-                                body={item.body}
-                            />
-                        )
-                    })}
-                </div>
-            </main>
-
-            <footer className={styles.footer}>
-                <a
-                    href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Powered by{' '}
-                    <span className={styles.logo}>
-                        <Image
-                            src='/vercel.svg'
-                            alt='Vercel Logo'
-                            width={72}
-                            height={16}
-                        />
-                    </span>
-                </a>
-            </footer>
-        </div>
+            {/* <div className={styles.cardWrapper}>
+                {data.map((item, index) => {
+                    return (
+                        <Card key={index} title={item.title} body={item.body} />
+                    )
+                })}
+            </div> */}
+        </Container>
     )
 }
 

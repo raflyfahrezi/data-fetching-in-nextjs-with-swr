2e2f32950ddece809f79219d5b0677d21a81b75a
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-05-20 15:48:39 +0700

    create container component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Head from './head'
 import Link from './link'
 import Card from './card'
+import Container from './container'
 
-export { Head, Link, Card }
+export { Head, Link, Card, Container }

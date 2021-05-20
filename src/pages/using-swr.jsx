23cdaf2267cb428566062c6a4d79a4cf7e7a131f
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-05-20 16:08:46 +0700

    done with swr pages

--- src/pages/using-swr.jsx
+++ src/pages/using-swr.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import useSWR from 'swr'
 
-import { Container } from '../components'
+import { fetcher } from '../utils'
+import { Card, Container } from '../components'
 
 import styles from '../styles/Home.module.css'
 
 const usingSwr = () => {
+    const { data } = useSWR(process.env.NEXT_PUBLIC_API_URL, fetcher)
+
     return (
         <Container title='Using SWR'>
             <h1 className={styles.title}>Using SWR</h1>
+
+            {data ? (
+                <div className={styles.cardWrapper}>
+                    {data.map((item, index) => {
+                        return (
+                            <Card
+                                key={index}
+                                title={item.title}
+                                body={item.body}
+                            />
+                        )
+                    })}
+                </div>
+            ) : (
+                <div>
+                    <p>Loading</p>
+                </div>
+            )}
         </Container>
     )
 }

cc716a2d239897b60b41d5d559edb13fc1c77a32
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-05-20 15:09:48 +0700

    showing data on using useEffect

--- src/pages/using-useeffect.jsx
+++ src/pages/using-useeffect.jsx
@@ -1,11 +1,20 @@
+import axios from 'axios'
 import Image from 'next/image'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { Head } from '../components'
+import { Head, Card } from '../components'
 
 import styles from '../styles/Home.module.css'
 
 const usingUseEffect = () => {
+    const [data, setData] = useState([])
+
+    useEffect(async () => {
+        const response = await axios.get(process.env.NEXT_PUBLIC_API_URL)
+
+        setData(response.data)
+    }, [])
+
     return (
         <div className={styles.container}>
             <Head title='Using useEffect' />
@@ -13,7 +22,18 @@ const usingUseEffect = () => {
             <main className={styles.main}>
                 <h1 className={styles.title}>Using UseEffect</h1>
 
-                <div></div>
+                <div>
+                    {data.length > 0 &&
+                        data.map((item, index) => {
+                            return (
+                                <Card
+                                    key={index}
+                                    title={item.title}
+                                    body={item.body}
+                                />
+                            )
+                        })}
+                </div>
             </main>
 
             <footer className={styles.footer}>

75f554b5e4a1b796c24bb69b13f4e780d5d6898b
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-05-20 16:03:10 +0700

    create using swr pages

---
+++ src/pages/using-swr.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Container } from '../components'
+
+import styles from '../styles/Home.module.css'
+
+const usingSwr = () => {
+    return (
+        <Container title='Using SWR'>
+            <h1 className={styles.title}>Using SWR</h1>
+        </Container>
+    )
+}
+
+export default usingSwr

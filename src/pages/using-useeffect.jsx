88164f917ddbe81315bfe494fbb7a3001a79a869
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-05-20 15:53:34 +0700

    implement container on using-useeffect

--- src/pages/using-useeffect.jsx
+++ src/pages/using-useeffect.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 
-import { Head, Card } from '../components'
+import { Card, Container } from '../components'
 
 import styles from '../styles/Home.module.css'
 
@@ -16,49 +16,27 @@ const usingUseEffect = () => {
     }, [])
 
     return (
-        <div className={styles.container}>
-            <Head title='Using useEffect' />
+        <Container title='Using useEffect'>
+            <h1 className={styles.title}>Using UseEffect</h1>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>Using UseEffect</h1>
-
-                {data.length > 0 ? (
-                    <div className={styles.cardWrapper}>
-                        {data.map((item, index) => {
-                            return (
-                                <Card
-                                    key={index}
-                                    title={item.title}
-                                    body={item.body}
-                                />
-                            )
-                        })}
-                    </div>
-                ) : (
-                    <div>
-                        <h2>Loading</h2>
-                    </div>
-                )}
-            </main>
-
-            <footer className={styles.footer}>
-                <a
-                    href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Powered by{' '}
-                    <span className={styles.logo}>
-                        <Image
-                            src='/vercel.svg'
-                            alt='Vercel Logo'
-                            width={72}
-                            height={16}
-                        />
-                    </span>
-                </a>
-            </footer>
-        </div>
+            {data.length > 0 ? (
+                <div className={styles.cardWrapper}>
+                    {data.map((item, index) => {
+                        return (
+                            <Card
+                                key={index}
+                                title={item.title}
+                                body={item.body}
+                            />
+                        )
+                    })}
+                </div>
+            ) : (
+                <div>
+                    <h2>Loading</h2>
+                </div>
+            )}
+        </Container>
     )
 }
 
